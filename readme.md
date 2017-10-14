ee293441deab78f9688bf6b2f224f2b0c3f77af9
trud <jeremy.vtrudel@cidco.ca>
2017-10-14 05:59:24 -0400

    init

---
+++ readme.md
@@ -0,0 +1,3 @@
+# C++ Factory
+
+Non, il ne s'agit pas du Design Pattern Factory... C'est plutôt une collection de templates permettant de générer du code c++.
---
+++ tests/struct/main.cpp
@@ -0,0 +1,16 @@
+#include "bottin_atom.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    db::pj::bottin_atom b1;
+    db::pj::bottin_atom b2;
+
+    cout <<"lt "<< (b1<b2)<<endl;
+
+
+
+}
